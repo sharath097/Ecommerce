7c9dc00e71aaa11afc0c87096cf62e6e89fa3143
sharath <sharathshar097@gmail.com>
2025-01-04 12:55:48 +0530

    product overview

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_product, name="get_product"),
+    path("<slug>/", views.get_product, name="get_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
-def get_product(request):
-    return render(request, "products/product.html")
+def get_product(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+        return render(request, "products/product.html", {"product": product})
+    except Exception as e:
+        print(e)
